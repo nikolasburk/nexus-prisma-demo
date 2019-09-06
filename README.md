b495d94454fe8d3a2381f36787427de8144ad48f
Nikolas Burk <nikolas.burk@gmail.com>
2019-09-06 16:30:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# GraphQL Day 2019: Code-first GraphQL Server Development with Prisma
+
+## Get started
+
+### 0. Install the Prisma 2 CLI
+
+```
+npm install -g prisma2
+```
+
+### 1. Clone the repository
+
+```
+git clone 
+cd nexus-demo
+```
+
+## 2. Install dependencies
+
+```
+npm install
+```
+
+> **Note**: The `prisma2 generate` command is added as a `postinstall` script to `package.json`, so this will also generate the required code for Photon.js and the `nexus-prisma`plugin.
+
+## 3. Run Prisma's development mode
+
+```
+prisma2 dev
+```
+
+> **Note**: This performs an initial database migration. To persist the migration into Lift's migration history, run `prisma2 lift save` and `prisma2 lift up` later.
+
+## 4. Start the GraphQL server
+
+```
+npm start
+```
+
+## 5. Explore the GraphQL API
+
+You can now open a GraphQL Playground at [`http://localhost:4000`](http://localhost:4000) and start sending queries and mutation to the API.
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { makeSchema, objectType, stringArg, idArg } from 'nexus'
+import Photon from '@generated/photon'
+import { nexusPrismaPlugin } from '@generated/nexus-prisma'
+
+const photon = new Photon()
+
+const nexusPrismaTypes = nexusPrismaPlugin({
+  photon: ctx => ctx.photon
+})
+
+const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.email()
+    t.model.name()
+    t.model.posts()
+  },
+})
+
+const Post = objectType({
+  name: 'Post',
+  definition(t) {
+    t.model.id()
+    t.model.title()
+    t.model.content()
+    t.model.published()
+    t.model.author()
+  },
+})
+
+const Query = objectType({
+  name: 'Query',
+  definition(t) {
+    t.crud.users()
+  },
+})
+
+const Mutation = objectType({
+  name: 'Mutation',
+  definition(t) {
+   t.crud.createOneUser({
+     alias: 'createUser'
+   })
+
+   t.crud.updateOneUser({
+    alias: 'updateUser'
+  })
+
+  t.crud.createOnePost({
+    alias: 'createDraft'
+  })
+   
+    t.field('publish', {
+      type: 'Post',
+      args: {
+        id: idArg(),
+      },
+      resolve: (_, args) => photon.posts.update({
+        where: { id: args.id },
+        data: { published: true }
+      })
+    })
+  },
+})
+
+const schema = makeSchema({
+  types: [Query, Mutation, User, Post, nexusPrismaTypes],
+
+  outputs: {
+    schema: __dirname + '/generated/schema.graphql',
+    typegen: __dirname + '/generated/nexus.ts',
+  },
+
+  nonNullDefaults: {
+    input: true,
+  },
+})
+
+const server = new GraphQLServer({ schema, context: { photon } })
+server.start(() => console.log(`Server running on http://localhost:4000`))
